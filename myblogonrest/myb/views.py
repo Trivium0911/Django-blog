910a98e086ef94b7e18d8858946232f217e21a4a
Trivium0911 <triv_1993@mail.ru>
2022-07-25 10:41:09 +0300

    added registration

--- myblogonrest/myb/views.py
+++ myblogonrest/myb/views.py
@@ -1,6 +1,9 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.contrib.auth import login
+from .forms import SigUpForm
 from .models import Post
 
 
@@ -14,4 +17,32 @@ class MainView(View):
 
         return render(request, 'myb/home.html', context={
             'page_obj': page_obj
+        })
+
+
+class PostDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myb/post_detail.html', context={
+            'post': post
+    })
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SigUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SigUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
         })
